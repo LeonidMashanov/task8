1c98ac4c5081ea52d8da762114b87a15ddddc8ab
LeonidMashanov <len4ik272523@gmail.com>
2020-12-24 14:20:38 +0200

    create class Dog, constructor,setter and getter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+class Dog {
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
